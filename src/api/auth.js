0946f355117285a990e87db514089393144d73b6
Berwyn Tan <berwyntan@gmail.com>
2023-03-30 16:52:36 +0800

    credentials to axios

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,16 +1,17 @@
-import axios from "axios"
+import axios from "axios";
 
 export const apiLogin = async (data) => {
-    try {
-      const response = await axios.post("http://127.0.0.1:8080/auth", data,
-        {
-            headers: { 'Content-Type': 'application/json' }
-        }
-      )
+  try {
+    const response = await axios.post("http://127.0.0.1:8080/auth", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    });
     //   console.log(response)
-      return response
-    } catch (error) {
-        console.log(error)
-        return error.response
-    }
-}
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error.response;
+  }
+};
